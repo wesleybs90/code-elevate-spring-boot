dbb734934daad953bd3cbad8e808ffef15dd6457
Wesley Soares <wesleybs90@gmail.com>
2025-03-22 17:27:26 -0300

    Adicionar retry para envio de mensagem Kafka

--- src/main/java/com/example/code_elevate/service/KafkaProducerService.java
+++ src/main/java/com/example/code_elevate/service/KafkaProducerService.java
@@ -1,6 +1,8 @@
 package com.example.code_elevate.service;
 
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +14,7 @@ public class KafkaProducerService {
     this.kafkaTemplate = kafkaTemplate;
   }
 
+  @Retryable(value = { Exception.class }, maxAttempts = 5, backoff = @Backoff(delay = 2000))
   public void sendMessage(String topic, String message) {
     kafkaTemplate.send(topic, message);
   }
