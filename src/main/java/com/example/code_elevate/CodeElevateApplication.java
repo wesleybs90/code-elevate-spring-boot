fa26cb41c3f41608fcf3d8f7f4e411356c0a3406
Wesley Soares <wesleybs90@gmail.com>
2025-03-25 20:52:23 -0300

    Ajustes para rodar no Azure Web Apps

--- src/main/java/com/example/code_elevate/CodeElevateApplication.java
+++ src/main/java/com/example/code_elevate/CodeElevateApplication.java
@@ -2,13 +2,14 @@ package com.example.code_elevate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableCaching
 @EnableRetry
-public class CodeElevateApplication {
+public class CodeElevateApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeElevateApplication.class, args);
